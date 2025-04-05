ef8b8a390ee41efcf4794aa17dc2d01049fa3e8d
mhijazi16 <gultimate0@gmail.com>
2025-04-05 11:36:11 +0300

    add ability to open postgres database

--- project-menu.sh
+++ project-menu.sh
@@ -6,10 +6,19 @@ projects["🥝 Infra Deployer"]="/home/ka1ser/coldrelay/infra_service/"
 projects["🫐 Hyprland Dotfiles"]="/home/ka1ser/.config/hypr/default/"
 projects["🌵 Wofi Scripts"]="/home/ka1ser/.config/wofi/scripts/"
 projects["🌋 Neovim Configs"]="/home/ka1ser/.config/nvim/"
+projects["🐫 Open Postgres"]="docker ps | grep \"postgres \" | awk '{print \$NF}'"
 
 project=$(printf "%s\n" "${!projects[@]}" | wofi -n -d -p "Search > ")
-path=("${projects[$project]}")
 
+path=("${projects[$project]}")
 if [[ -n "$path" ]]; then
-  cd ${path} && nohup neovide . && disown && exit
+  if [[ "$project" == "🐫 Open Postgres" ]]; then
+    postgres
+    container_name=$(eval "${projects["$project"]}")
+    kitty docker exec -it "$container_name" psql -U postgres
+  else
+    source /home/ka1ser/global/bin/activate
+    cd "$path" && nohup neovide . &
+    disown && exit
+  fi
 fi
