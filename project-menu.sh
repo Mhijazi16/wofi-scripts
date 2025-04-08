f615db8859bc3f8e28600dc757243906d5df8329
mhijazi16 <gultimate0@gmail.com>
2025-04-08 14:48:56 +0300

    fixes to open postgres

--- project-menu.sh
+++ project-menu.sh
@@ -2,6 +2,7 @@
 
 declare -A projects
 projects["🧊 ColdRelay Core"]="/home/ka1ser/coldrelay/core_service/"
+projects["💰 Subscription Service"]="/home/ka1ser/coldrelay/subscription_management_service//"
 projects["🥝 Infra Deployer"]="/home/ka1ser/coldrelay/infra_service/"
 projects["🫐 Hyprland Dotfiles"]="/home/ka1ser/.config/hypr/default/"
 projects["🌵 Wofi Scripts"]="/home/ka1ser/.config/wofi/scripts/"
@@ -9,13 +10,16 @@ projects["🌋 Neovim Configs"]="/home/ka1ser/.config/nvim/"
 projects["🐫 Open Postgres"]="docker ps | grep \"postgres \" | awk '{print \$NF}'"
 projects["👺 ColdRelay ENV"]="neovide /home/ka1ser/coldrelay/core_service/.env"
 projects["👺 Infra ENV"]="neovide /home/ka1ser/coldrelay/infra_service/.env"
+projects["👺 Subscription ENV"]="neovide /home/ka1ser/coldrelay/subscription_management_service/.env"
 
 project=$(printf "%s\n" "${!projects[@]}" | wofi -n -d -p "Search > ")
 
 path=("${projects[$project]}")
 if [[ -n "$path" ]]; then
   if [[ "$project" == "🐫 Open Postgres" ]]; then
-    postgres
+    docker run -p 5432:5432 -e POSTGRES_PASSWORD=secret \
+      -v postgres_data:/var/lib/postgresql/data \
+      -d postgres
     container_name=$(eval "${projects["$project"]}")
     kitty docker exec -it "$container_name" psql -U postgres
   elif [[ "$project" == *"ENV"* ]]; then
