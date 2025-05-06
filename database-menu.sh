f54d1e749789d48ee5a782991b59183712d2cb7d
mhijazi16 <gultimate0@gmail.com>
2025-05-06 18:33:54 +0300

    create database menu

---
+++ database-menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+docker start coldrelay
+query="SELECT admin_domain, admin_password FROM core.servers;"
+data=$(docker exec -it coldrelay psql -U postgres -d coldrelay -P pager=off -c "$query" --csv)
+
+declare -A servers 
+
+while IFS= read -r line; do
+  key=$(echo "$line" | cut -d ',' -f1)
+  value=$(echo "$line" | cut -d ',' -f2-)
+  servers["$key"]="$value"
+done <<< "$data"
+
+# Show domain list in wofi
+selected=$(printf "%s\n" "${!servers[@]}" | wofi --dmenu --prompt "Search > ")
+echo ${servers["$selected"]} | wl-copy
+notify-send "copied password for $selected"
